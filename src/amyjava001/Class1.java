1b5bb12465d26e69a48caa748dbebfa259b97480
Naresh <halchal14naresh@gmail.com>
2022-10-01 18:51:43 +0530

    Hellow

---
+++ src/amyjava001/Class1.java
@@ -0,0 +1,23 @@
+package amyjava001;
+
+ 
+import java.util.ArrayList;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+ @Test 
+  //@Parameters ({"mypara"})
+ //public void method1(@Optional String a){
+	 public void method1(  ){
+	 ArrayList<String> ar = new ArrayList<String>();
+	 ar.add("Naresh");
+	ar.add("Kumar");
+	ar.add("Yadav");
+	 System.out.println(ar.toString().replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", "").trim());
+ }
+
+}
