cb2d349b2c461aacbb79a7c77e2b5827b5f8d048
Poonam <poonamyadavsnbp@gmail.com>
2022-10-17 10:35:00 +0530

    saxsax

--- src/myjava/MyChild.java
+++ src/myjava/MyChild.java
@@ -15,35 +15,9 @@ static public void myStatic(){
 	}
  
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		
-		
-		// Parent can hold child but child can not hold parent
-		
-
- 
-		
-
-		System.out.println("PIZZA");
-
-=======
-	public static void main(String[] args) {	
->>>>>>> refs/remotes/origin/main/naresh/auto002
+public static void main(String[] args) {	
 		Parent1 par = new MyChild();
-<<<<<<< HEAD
-		
-		System.out.println("PIZZA HI RAHEGA");
-		
-	 
-		
-		
-		
-		
-		
-=======
-		System.out.println("SAMOSA");		
->>>>>>> refs/remotes/origin/main/naresh/auto002
+		System.out.println("SAMOSA");		 
 	}
 
 }
